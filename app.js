b235b71deeabd407908fd26489d5139a524d77bc
George <bl1723r@gmail.com>
2019-03-12 21:19:15 +0200

    init

---
+++ app.js
@@ -0,0 +1,49 @@
+// Variables
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = "#BADA55";
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 30;
+//ctx.globalCompositeOperation = 'overlay';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+
+// Draw function
+function draw(e) {
+    if (!isDrawing) return; // stop the function when not mouse click down
+    document.querySelector('h1').style.display = "none";
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    hue++;
+    if (hue >= 360) {
+        hue = 0;
+    }
+    if (ctx.lineWidth >= 50 || ctx.lineWidth <= 1) {
+        direction = !direction;
+    }
+    if (direction) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;   
+    }
+}
+
+// Event listeners
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', (e) => { 
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];  
+});
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
